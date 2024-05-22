24eba48b4daebaf49a118f5332cb6d0f594fac2c
Aleksandr Bankevich <aleksandr@bankevich.com>
2024-05-23 00:05:25 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lombok.SneakyThrows;
+
+public class Main extends Application {
+
+
+
+    @SneakyThrows
+    public void start(Stage stage)  {
+
+        Parent root = FXMLLoader.load(Main.class.getClassLoader().getResource("Templates.main.fxml"));
+        Scene scene = new Scene(root, 700, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        //sdfsdf
+        launch(args);
     }
 }
