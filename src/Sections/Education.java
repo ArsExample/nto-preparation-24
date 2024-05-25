14578239f2778777f8ad3443e4e1e350a5640f66
Aleksandr Bankevich <aleksandr@bankevich.com>
2024-05-26 00:54:03 +0300

    1

--- src/Sections/Education.java
+++ src/Sections/Education.java
@@ -1,4 +1,16 @@
 package Sections;
 
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Education")
+@Data
 public class Education {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
 }
